1ec19678185c99cb20f42059819f2056da828ae6
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-10-10 20:45:06 +0100

    Built the header options

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React from 'react';
-import './App.css';
-import Header from './Header';
+import React from "react";
+import "./App.css";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="app">
-      {/* {Header} */}
       <Header />
 
       {/* {App Body} */}
-        {/* {Sidebar} */}
-        {/* {Feed} */}
-        {/* {Widgets} */}
+      <div className="app__body">
 
+      </div>
+      {/* {Sidebar} */}
+      {/* {Feed} */}
+      {/* {Widgets} */}
     </div>
   );
 }
--- src/Header.js
+++ src/Header.js
@@ -1,26 +1,37 @@
-import React from 'react'
-import './Header.css'
-import SearchIcon from '@mui/icons-material/Search';
+import React from "react";
+import "./Header.css";
+import SearchIcon from "@mui/icons-material/Search";
+import HeaderOption from "./HeaderOption";
+import HomeIcon from "@mui/icons-material/Home";
+import SupervisorAccountIcon from "@mui/icons-material/SupervisorAccount";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import ChatIcon from "@mui/icons-material/Chat";
+import NotificationsIcon from "@mui/icons-material/Notifications";
 
 function Header() {
   return (
     <div className="header">
-        <div className="header__left">
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png" 
-            alt="" 
-          />
+      <div className="header__left">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/3536/3536505.png"
+          alt=""
+        />
 
-          <div className="header__search">
-            <SearchIcon />
-            <input type="text" />
-          </div>
+        <div className="header__search">
+          <SearchIcon />
+          <input type="text" />
         </div>
-        <div className="header__right">
-          
-        </div>
+      </div>
+      <div className="header__right">
+        <HeaderOption Icon={HomeIcon} title="Home" />
+        <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+        <HeaderOption Icon={BusinessCenterIcon} title="Jobs" />
+        <HeaderOption Icon={ChatIcon} title="Messaging" />
+        <HeaderOption Icon={NotificationsIcon} title="Notifications" />
+        <HeaderOption avatar="https://cameron-personal-portfolio.web.app/images/portrait.png" title="me" />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Header
+export default Header;
