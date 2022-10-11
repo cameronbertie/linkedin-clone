3701e0a859a45a014067673452f74e51b98d0161
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-10-11 19:44:35 +0100

    Built the sidebar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./Header";
+import Sidebar from "./Sidebar";
 
 function App() {
   return (
@@ -9,11 +10,11 @@ function App() {
 
       {/* {App Body} */}
       <div className="app__body">
+        <Sidebar />
 
+        {/* {Feed} */}
+        {/* {Widgets} */}
       </div>
-      {/* {Sidebar} */}
-      {/* {Feed} */}
-      {/* {Widgets} */}
     </div>
   );
 }
