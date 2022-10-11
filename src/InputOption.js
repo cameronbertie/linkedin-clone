a27f20f0d264a3194f5779cd4f503c7183ba6b3c
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-10-11 22:01:57 +0100

    Built the post section

--- src/InputOption.js
+++ src/InputOption.js
@@ -1,13 +1,13 @@
-import React from 'react'
-import "./InputOption.css"
+import React from "react";
+import "./InputOption.css";
 
-function InputOption({Icon, title, color}) {
+function InputOption({ Icon, title, color }) {
   return (
     <div className="inputOption">
-      <Icon style={{color: color}} />
+      <Icon style={{ color: color }} />
       <h4>{title}</h4>
     </div>
-  )
+  );
 }
 
-export default InputOption
+export default InputOption;
