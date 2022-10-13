261867be3692e9468581d30890fdc5184c8d882b
Cameron Bertie <72890283+cameronbertie@users.noreply.github.com>
2022-10-13 18:33:29 +0100

    Integrated Redux

--- src/App.js
+++ src/App.js
@@ -3,17 +3,26 @@ import "./App.css";
 import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
+import { useSelector } from "react-redux";
+import { selectUser } from "./features/userSlice";
 
 function App() {
+const user = useSelector(selectUser)
+
+
   return (
     <div className="app">
       <Header />
 
+      {!user ? (
+      <Login />
+      ) : (
       <div className="app__body">
         <Sidebar />
         <Feed />
         {/* {Widgets} */}
       </div>
+      )}
     </div>
   );
 }
